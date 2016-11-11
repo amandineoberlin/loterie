2dec80c2d6082ba251f360cd68a78a373fd83144
AmandineOberlin <amandineoberlin@MacBook-Pro-de-AmandineOberlin.local>
2016-11-12 00:26:46 +0100

    11Nov

--- script.js
+++ script.js
@@ -39,35 +39,34 @@ $(document).ready(function(){
       $('#prenom').html(name);
       $('#valider').click(function(event) {
         event.preventDefault();
-
-        $.ajax({
-          type: "POST",
-          url: "cible.php",
-          data: 'name='+ $( "#select" ).val(),
-          success: function(result){
-            result = JSON.parse(result);
-            $('#resultat').css({ display: 'block'});
-            //result is array which means user already played
-            if ($.isArray(result.resultat)) {
-              $('#resultat').html('');
-              $('#resultat').html('Tu as tiré au sort ' + result.resultat);
-            } else {
-              $('#resultat').html('');
+        $('.loader').css({ display: 'block' });
+        setTimeout(function(){
+          $('.loader').css({ display: 'none' });
+          $.ajax({
+            type: "POST",
+            url: "cible.php",
+            data: 'name='+ $( "#select" ).val(),
+            success: function(result){
+              result = JSON.parse(result);
+              $('#resultat').css({ display: 'block'});
               $('#resultat').html('Tu as tiré au sort : ' + result.resultat);
               $('#tableau').html('<h3>' + getCurrentTable(result.file) + ' participant(s) sur 5 validé(s)' + '</h3>'
               + '</h6>La liste des résultats sera visible lorsque tout le monde aura joué</h6>');
-            }
-            if (getCurrentTable(result.file) === 5) {
-              tbl_row = '<table class="table"><tr><th>Nom</th><th>offre à : </th></tr>';
-                $.each(result.file, function(k, v) {
-                  tbl_row += '<tr><td>' + k + '</td><td>' + v + '</td></tr>'
-                });
-              tbl_row += '</table>';
-              $('#tableau').replaceWith(tbl_row);
-              }
 
-            }
-        });
+              if (getCurrentTable(result.file) === 5) {
+                tbl_row = '<table class="table"><tr><th>Nom</th><th>offre à : </th></tr>';
+                  $.each(result.file, function(k, v) {
+                    tbl_row += '<tr><td>' + k + '</td><td>' + v + '</td></tr>'
+                  });
+                tbl_row += '</table>';
+                $('#tableau').replaceWith(tbl_row);
+                }
+
+              }
+          });
+        }, 2000);
+
+
       })
     }
 
